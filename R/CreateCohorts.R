99b8818ef95513f59a1cfc0c2e0afbdf01c0e049
Solon Ioannou <sioann88@gmail.com>
2024-02-19 16:26:27 +0100

    Updating createCohorts()

--- R/CreateCohorts.R
+++ R/CreateCohorts.R
@@ -2,29 +2,38 @@
 createCohorts <- function(connectionDetails, 
                           analysisDetails){
   
-  cohortTable <- analysisDetails$cohortTable
-  cdmDatabaseSchema <- analysisDetails$cdmDatabaseSchema
-  cohortDatabaseSchema <- analysisDetails$cohortDatabaseSchema
-  incremental <- analysisDetails$incremental
+  cohortTable <- analysisDetails$cdmSettings$cohortTable
+  cdmDatabaseSchema <- analysisDetails$cdmSettings$cdmDatabaseSchema
+  cohortDatabaseSchema <- analysisDetails$cdmSettings$cohortDatabaseSchema
+  cohortDefinitionSet <- analysisDetails$cdmSettings$cohortDefinitionSet
+  incremental <- analysisDetails$cdmSettings$incremental
   projectRoot <- system.file(package = "TreatmentPatternsOfChronicCough")
   
   
-  cohortDefinitionSet <- CohortGenerator::getCohortDefinitionSet(settingsFileName = file.path(projectRoot, "settings", "CohortsToCreate.csv"), 
-                                                                jsonFolder = file.path(projectRoot,"cohorts"), 
-                                                                sqlFolder = file.path(projectRoot, "sql", "sql_server")
-                                                                )
+  # cohortDefinitionSet <- CohortGenerator::getCohortDefinitionSet(settingsFileName = file.path(projectRoot, "settings", "CohortsToCreate.csv"), 
+  #                                                               jsonFolder = file.path(projectRoot,"cohorts"), 
+  #                                                               sqlFolder = file.path(projectRoot, "sql", "sql_server")
+  #                                                               )
   
   cohortTableNames <- CohortGenerator::getCohortTableNames(cohortTable = cohortTable)
   
   CohortGenerator::createCohortTables(connectionDetails = connectionDetails, 
                                       cohortDatabaseSchema = cohortDatabaseSchema, 
-                                      cohortTableNames = cohortTableNames, incremental = FALSE)
+                                      cohortTableNames = cohortTableNames,
+                                      incremental = incremental)
   
   CohortGenerator::generateCohortSet(connectionDetails = connectionDetails,
                                      cdmDatabaseSchema = cdmDatabaseSchema, 
                                      cohortDatabaseSchema = cohortDatabaseSchema,
                                      cohortTableNames = cohortTableNames, 
                                      cohortDefinitionSet = cohortDefinitionSet, 
-                                     incremental = FALSE)
+                                     incremental = incremental)
+  cohortCounts <- CohortGenerator::getCohortCounts(connectionDetails = connectionDetails,
+                                                   cohortDatabaseSchema = cohortDatabaseSchema, 
+                                                   cohortTable = cohortTable)
+  
+  message(paste0("Cohorts created."))
+  print(cohortCounts)
+  
   return(invisible())
 }
