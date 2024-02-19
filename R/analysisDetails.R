cba1271ff8a0808a06b4ceb8157bb52283bde995
Solon Ioannou <sioann88@gmail.com>
2024-02-19 16:27:02 +0100

    Updating analysisDetails()

--- R/analysisDetails.R
+++ R/analysisDetails.R
@@ -1,30 +1,44 @@
 #' @export
-createAnalysisDetails <- function(cdmDatabaseSchema,
-                                  cohortDatabaseSchema,
+createAnalysisDetails <- function(connection = NULL, 
+                                  cdm = NULL, 
+                                  cdmDatabaseSchema = NULL,
+                                  cohortDatabaseSchema = NULL,
+                                  resultsDatabaseSchema = NULL, 
                                   cohortTableNames, 
                                   cohortDefinitionSet,
                                   cohortTable, 
                                   databaseId, 
-                                  minCellCount, 
                                   baseUrl, 
-                                  saveSettings, 
+                                  incremental = FALSE, 
                                   databaseName, 
+                                  cohorts, 
+                                  treatmentPattersSettings, 
                                   diagnosticsExportFolder,
                                   treatmentPatternsExportFolder){
   
-  settings <- list(cdmDatabaseSchema = cdmDatabaseSchema, 
-                   cohortDatabaseSchema = cohortDatabaseSchema, 
-                   cohortTableNames = cohortTableNames, 
-                   cohortDefinitionSet = cohortDefinitionSet, 
-                   cohortTable = cohortTable, 
-                   databaseId = databaseId, 
-                   minCellCount = minCellCount, 
-                   baseUrl = baseUrl, 
-                   saveSettings = saveSettings, 
-                   databaseName = databaseName,
-                   diagnosticsExportFolder = diagnosticsExportFolder, 
-                   treatmentPatternsExportFolder = treatmentPatternsExportFolder
-                   )
+  settings <- vector("list", 2)
+  names(settings) <- c("cdmSettings", "cohortSettings")
+  settings$cdmSettings <- list(connection = connection, 
+                               cdm = cdm, 
+                               cdmDatabaseSchema = cdmDatabaseSchema, 
+                               resultsDatabaseSchema = resultsDatabaseSchema,
+                               cohortDatabaseSchema = cohortDatabaseSchema, 
+                               cohortTableNames = cohortTableNames, 
+                               cohortDefinitionSet = cohortDefinitionSet, 
+                               cohortTable = cohortTable, 
+                               databaseId = databaseId, 
+                               baseUrl = baseUrl, 
+                               incremental = incremental, 
+                               databaseName = databaseName)
+  settings$cohortSettings <- vector("list", length(cohorts))
+  for (i in seq_along(cohorts)) {
+    settings$cohortSettings[[i]] <- list(cohorts = cohorts[[i]],
+                                         treatmentPattersSettings = treatmentPattersSettings[[i]],
+                                         diagnosticsExportFolder = file.path(diagnosticsExportFolder, paste0(names(cohorts[i]))), 
+                                         treatmentPatternsExportFolder = file.path(treatmentPatternsExportFolder, paste0(names(cohorts[i])))
+    )       
+  }
+  names(settings$cohortSettings) <- names(cohorts)
   
   return(settings)
 }
